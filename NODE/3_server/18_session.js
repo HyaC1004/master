acb76e81e01c5df8e74e97cdcb4122cc8f78b044
HyaC1107 <98933239+HyaC1107@users.noreply.github.com>
2022-07-12 09:20:35 +0900

    0711

--- NODE/3_server/18_session.js
+++ NODE/3_server/18_session.js
@@ -9,58 +9,63 @@ const ejs = require("ejs");
 
 const sessions = {};
 const namePool = [];
-http.createServer(async(req, res) => {
+
+http.createServer(async (req, res) => {
     if(req.url==="/favicon.ico") {
         res.statusCode=404;
         return res.end();
     }
-    // console.log(uuid.v4());  // 강력한 unique 한 값이 만들어진다 이걸 가지고 세션키값으로 사용해보자.
-    const recvCookie = cooikeParser(req.headers.cookie);
+    const recvCookie = cookieParser(req.headers.cookie);
     let currentUserSession;
     if(!recvCookie.sessionId) {
         const uk = uuid.v4();
-        sessions[uk] = { };        
+        sessions[uk] = { };
         res.setHeader("set-cookie", "sessionId="+uk);        
         currentUserSession = sessions[uk];
-    } else {        
-        console.log(recvCookie.sessionId,sessions);
+    } else {
+        if(sessions[recvCookie.sessionId] === undefined) {
+            sessions[recvCookie.sessionId]= {};
+        }
         currentUserSession = sessions[recvCookie.sessionId];
     }
-    
-    
     //=====================================================
+    console.log(currentUserSession);
     if(req.url==="/game") {
         if(!currentUserSession.playerName) {
-            res.writeHead(302,{"location":"/login"});
+            res.writeHead(302, {"location":"/login"});
             return res.end();
-        }else{
-            let html = await ejs.renderFile(path.join(__dirname,"view","game.ejs"),{currentUserSession});
-            return res.end(html)
+        }else {
+            let html = await ejs.renderFile(path.join(__dirname,"view","game.ejs"), {currentUserSession});
+            return res.end(html);
         }
-    } else if(req.url==="/login") {
-        let html = await ejs.renderFile(path.join(__dirname,"view","login.ejs"),{msg:""});
-        return res.end(html)
+    } else if(req.url ==="/login") {
+        let html = await ejs.renderFile(path.join(__dirname,"view","login.ejs"), { msg : ""} );
+        return res.end(html);
     } else if(req.url.startsWith("/session")) {
-        let query = url.parse(req.url,true).query;
-        // form에서 전달받을때 input name설정을 name으로 한걸 가정
-        if(namePool.includes(query.name)){
-            let html = await ejs.renderFile(path.join(__dirname,"view","login.ejs"),
-                            {msg:"이미 사용중인 이름입니다."});
-            return res.end(html)
-        }else{
-            namePool.push(query.nmae);
+        let query = url.parse(req.url, true).query;
+        // form 에서 전달받을때 input name 설정을 name 으로 한걸 가정
+        if(namePool.includes(query.name)) {
+            let html = 
+                await ejs.renderFile(path.join(__dirname,"view","login.ejs") , 
+                                                 { msg : "이미 사용중인 이름입니다."} );
+            return res.end(html);
+        } else {
+            namePool.push(query.name);
             currentUserSession.playerName = query.name;
-            res.writeHead(302,{"location":"/game"});
+            res.writeHead(302, {"location":"/game"});
             return res.end();
         }
     }
+
+
+
 
 }).listen(8080, () => {
     console.log("[SERVER] STARTED");
 })
 
 
-function cooikeParser(str) {
+function cookieParser(str) {
     const cookies = {};
     if (str) {
         const cookieArray = str.split(/;\s+/);
